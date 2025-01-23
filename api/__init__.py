91393bd2824fbfd4c16ba6670235557583dc2169
Chintana Meegamarachchi <chintana@appsolve.com.au>
2025-01-23 09:20:04 +0000

    Adds countries endpoint

--- api/__init__.py
+++ api/__init__.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from api.customers.routes import customer_router
+from api.countries.routes import country_router
 
 app = FastAPI()
 
 app.include_router(customer_router, prefix="/customers")
+app.include_router(country_router, prefix="/countries")
